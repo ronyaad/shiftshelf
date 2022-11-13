49bbd043abcbed170a233778d62ddbe782e55535
ronyaad <38158472+ronyaad@users.noreply.github.com>
2022-11-13 22:03:41 +0200

    Do flask app

---
+++ app.py
@@ -0,0 +1,91 @@
+from flask import Flask
+import matplotlib.pyplot as plt
+from detecto import core, utils, visualize
+from detecto.core import Model
+import os
+import helpers
+
+app = Flask(__name__)
+
+import logging
+import os,shutil
+from flask import Flask, flash, render_template, request, send_from_directory,url_for
+from flask_uploads import IMAGES, UploadSet, configure_uploads
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.urandom(24)
+app.config["UPLOADED_PHOTOS_DEST"] = "uploads"
+
+photos = UploadSet("photos", IMAGES)
+configure_uploads(app, photos)
+
+class UploadForm(FlaskForm):
+    folder = app.config["UPLOADED_PHOTOS_DEST"]
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+    photo = FileField(validators=[FileAllowed(photos, 'Only images are allowed'),FileRequired('File field should not be empty')])
+    submit = SubmitField('Upload')
+
+class ObjectDetectionModel():
+    def __init__(self):
+        self.labels = ['Nescafe-Glass-Red',
+            'Nescafe-Bag-2in1',
+            'Nescafe-Bag-Cappuccino',
+            'Nescafe-Glass-GoldDecaf',
+            'Nescafe-Glass-Yellow',
+            'Tchibo-Glass-Gold',
+            'Nestle-Plastic-CoffeeMate',
+            'Nestle-Bag-CoffeeMate']
+    def getModel(self):
+        return Model.load('detecto.pth', self.labels)
+
+def get_plan(filename):
+    planogram = [['Nescafe-Bag-2in1', 'Nescafe-Bag-2in1'],
+                ['Nescafe-Bag-Cappuccino', 'Nescafe-Glass-Yellow'],
+                ['Nescafe-Glass-GoldDecaf',
+                'Nestle-Bag-CoffeeMate',
+                'Nestle-Plastic-CoffeeMate']]
+    obj = ObjectDetectionModel()
+    return helpers.getDifferenceGrid(obj.getModel(), utils.read_image(app.config["UPLOADED_PHOTOS_DEST"] + "/" +filename), planogram)
+   
+    
+@app.route('/uploads/<filename>')
+def get_file(filename):
+    if os.path.exists("result.png"):
+        os.remove("result.png")
+    images= []
+    image = utils.read_image(app.config["UPLOADED_PHOTOS_DEST"] + "/" +filename)
+    images.append(image)
+    obj = ObjectDetectionModel()
+    helpers.plot_prediction_grid_with_ImageSave(obj.getModel(),images, figsize=(16, 12))
+    return send_from_directory('','result.png')
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = photos.save(form.photo.data)
+        file_url = url_for('get_file', filename= filename)
+        plan = get_plan(filename)
+    else:
+        file_url = None
+        plan = None
+    return render_template('upload.html', form=form, file_url=file_url, plan=plan)
+
+def main():
+    """Run the Flask app."""
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
--- helpers.py
+++ helpers.py
@@ -1,13 +1,20 @@
 import pandas as pd
 import numpy as np
-
+import torch
+from torchvision import transforms
+import matplotlib
+import matplotlib.patches as patches
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from detecto.utils import reverse_normalize
+from detecto.core import Model
 def getGrid(labels, boxes):
     
     df = pd.DataFrame(boxes, columns = ['xmin', 'ymin', 'xmax', 'ymax'])
     df['labels']= labels
 
     df = df.sort_values('ymax')
-    df.loc[(df.ymax.shift() < df.ymax - df.ymax * 0.1),'group'] = 1 # everytime the jump betweeen two row is more than 20
+    df.loc[(df.ymax.shift() < df.ymax - np.std(df.ymax)),'group'] = 1 # everytime the jump betweeen two row is more than 20
     # use cumsum, ffill and fillna to complete the column group and have a different number for each one
     df['group'] = df['group'].cumsum().ffill().fillna(0)
 
@@ -21,7 +28,7 @@ def getGrid(labels, boxes):
     
     return grid
 
-def getDifferenceGrid(labels: list, boxes: list, scores: list, planogram: list) -> list:
+def getDifferenceGrid1(labels: list, boxes: list, scores: list, planogram: list) -> list:
     
     # Remove low accuracy items
     n = len([score for score in scores if score>0.6])
@@ -41,3 +48,77 @@ def getDifferenceGrid(labels: list, boxes: list, scores: list, planogram: list) 
                 wrong_items.append({correctItem:(shelf_index, item_index)})
     
     return wrong_items
+
+def getDifferenceGrid(model: Model, image, planogram: list = []) -> list:
+
+    labels, boxes, scores = model.predict(image)
+    # Remove low accuracy items
+    n = len([score for score in scores if score>0.6])
+
+    # Construct grid
+    grid = getGrid(labels[:n], boxes[:n])
+
+    wrong_items = []
+    # Iterating over shelves
+    for shelf_index in range(len(planogram)):
+        # Iterating over items in each shelf
+        for item_index in range(len(planogram[shelf_index])):
+            correctItem = planogram[shelf_index][item_index]
+            try:
+                gridItem = grid[shelf_index][item_index]
+            except:
+                gridItem = ""
+            if correctItem != gridItem:
+                wrong_items.append({correctItem:(shelf_index, item_index)})
+    
+    return wrong_items
+
+def plot_prediction_grid_with_ImageSave(model, images, dim=None, figsize=None, score_filter=0.6):
+    # If not specified, show all in one column
+    if dim is None:
+        dim = (len(images), 1)
+
+    if dim[0] * dim[1] != len(images):
+        raise ValueError('Grid dimensions do not match size of list of images')
+
+    fig, axes = plt.subplots(dim[0], dim[1], figsize=figsize)
+
+    # Loop through each image and position in the grid
+    index = 0
+    for i in range(dim[0]):
+        for j in range(dim[1]):
+            image = images[index]
+            preds = model.predict(image)
+
+            # If already a tensor, reverse normalize it and turn it back
+            if isinstance(image, torch.Tensor):
+                image = transforms.ToPILImage()(reverse_normalize(image))
+            index += 1
+
+            # Get the correct axis
+            if dim[0] <= 1 and dim[1] <= 1:
+                ax = axes
+            elif dim[0] <= 1:
+                ax = axes[j]
+            elif dim[1] <= 1:
+                ax = axes[i]
+            else:
+                ax = axes[i, j]
+
+            ax.imshow(image)
+
+            # Plot boxes and labels
+            for label, box, score in zip(*preds):
+                if score >= score_filter:
+                    width, height = box[2] - box[0], box[3] - box[1]
+                    initial_pos = (box[0], box[1])
+                    rect = patches.Rectangle(initial_pos, width, height, linewidth=1,
+                                             edgecolor='r', facecolor='none')
+                    ax.add_patch(rect)
+
+                    ax.text(box[0] + 5, box[1] - 10, '{}: {}'
+                            .format(label, round(score.item(), 2)), color='red')
+                ax.set_title('Image {}'.format(index))
+    plt.savefig('result.png')
+    plt.close()
+    path = 'result.png'
